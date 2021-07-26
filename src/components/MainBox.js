1b34087f7776d78cdb91d97ebca959a24f7220b6
IsaacCodes2021 <84209008+IsaacCodes2021@users.noreply.github.com>
2021-07-26 08:58:34 -0700

    completed dq

--- src/components/MainBox.js
+++ src/components/MainBox.js
@@ -1,22 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import MenuBar from "./MenuBar";
 import { Profile, Photos, Cocktails, Pokemon } from "./pages";
 
 function MainBox() {
-  /*
-    Replace the code below! Depending on what menu item is selected in the menu, 
-    I should render either a Profile, Photos, Cocktails, or Pokemon component.
-    Think of a way to track which menu item was selected. 
-    - Which component should have state? 
-    - Which component should have methods to control state? 
-    - Where should these methods be called?
-  */
 
-  let detailsToDisplay = <div>Hi, I'm a div!</div>;
+  let [activeItem, displayItem] = useState(<Profile/>)
+
+  function clickHandle(event) {  
+    if(event.target.id === "profile") displayItem(<Profile />)
+    else if(event.target.id === "picture") displayItem(<Photos />)
+    else if(event.target.id === "coctail") displayItem(<Cocktails />)
+    else if(event.target.id === "bird") displayItem(<Pokemon />)
+  }
+
+  let detailsToDisplay = <div>{activeItem}</div>;
 
   return (
     <div>
-      <MenuBar />
+      <MenuBar clickEvent = {clickHandle}/>
       {detailsToDisplay}
     </div>
   );
--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,32 +1,21 @@
 import React from "react";
 
-function MenuBar(props) {
-  /*
-
-  The 'span' tags below are the menu items. Think about the way a menu 
-  should work. When you click a menu item, the button typically becomes
-  'active' to indicate that it is currently selected. How could we achieve
-  this programatically? What other behavior do we expect when we click
-  on a menu item? Do we need state in this component, and if not, how can
-  this component be made aware of what is currently the active menu item?
-
-  */
-
+function MenuBar({clickEvent}) {  
   return (
-    <div className="ui four item menu">
-      <span className="item active">
+    <div className="ui four item menu" >
+      <span className="item active" onClick={clickEvent} id={'profile'}>
         <i className="user large icon" />
       </span>
 
-      <span className="item">
+      <span className="item" onClick={clickEvent} id={'picture'}>
         <i className="photo large icon" />
       </span>
 
-      <span className="item">
+      <span className="item" onClick={clickEvent} id={'coctail'}>
         <i className="cocktail large icon" />
       </span>
 
-      <span className="item">
+      <span className="item" onClick={clickEvent} id={'bird'}>
         <i className=" themeisle large icon" />
       </span>
     </div>
